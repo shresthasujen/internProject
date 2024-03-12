7d44a8693480c046530b3a1beb2d7037d01ee399
Ronesh <nakarmi.ronesh@gmail.com>
2024-03-12 16:41:26 +0545

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var swiper = new Swiper(".card-slider", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-next",
+    prevEl: ".swiper-prev",
+  },
+});
